8ebe8b29e1ac512965f821b695b232686dd6cc8e
theigorp <mr.igor187@gmail.com>
2025-05-30 09:43:20 +0200

    week3/max_salary: solve the problem

--- module_1_starter_files/week3_greedy_algorithms/7_maximum_salary/max_salary.js
+++ module_1_starter_files/week3_greedy_algorithms/7_maximum_salary/max_salary.js
@@ -1,69 +1,45 @@
 // by Igor Pavkovic
 
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// const inputs = [];
+const inputs = [];
 
-// rl.on('line', (line) => {
-//   inputs.push(line.trim());
+rl.on('line', (line) => {
+  inputs.push(line.trim());
 
-//   if (inputs.length === 2) {
-//     const n = parseInt(inputs[0], 10);
-//     const digits = inputs[1].split(' ').map(Number);
+  if (inputs.length === 2) {
+    const n = parseInt(inputs[0], 10);
+    const digits = inputs[1].split(' ').map(Number);
 
-//     if (digits.length !== n) {
-//       console.error(`Expected ${n} digits, but got ${digits.length}`);
-//       process.exit(1);
-//     }
+    if (digits.length !== n) {
+      console.error(`Expected ${n} digits, but got ${digits.length}`);
+      process.exit(1);
+    }
 
-//     const result = maxSalary(n, digits);
-//     console.log(result);
-//     process.exit();
-//   }
-// });
+    const result = maxSalary(n, digits);
+    console.log(result);
+    process.exit();
+  }
+});
 
 function maxSalary(n, numbers) {
-  numbers.sort((a, b) => b - a);
-  const stringNumbers = numbers.map((number) => String(number));
-  const largestNumber = stringNumbers[0];
-
-  // stringNumbers.sort((a,b) => b[1] - a[1]);
-  // return stringNumbers;
-
-  for (let i = 0; i < largestNumber.length; i++) {
-    // for (const number of stringNumbers) {
-    //   console.log(number, i);
-    // }
-    stringNumbers.sort((a, b) => {
-      console.log('i', i);
-      console.log('a', a);
-      console.log('b', b);
-      let aIndex = i;
-      let bIndex = i;
-
-      if (!a[i]) {
-        console.log('aIndex changed');
-        aIndex = a.length - 1;
-      }
-      if (!b[i]) {
-        console.log('bIndex changed');
-        bIndex = b.length - 1;
-      }
-
-      console.log('aIndex', aIndex);
-      console.log('bIndex', bIndex);
-      return b[bIndex] > a[aIndex] ? 1 : -1;
-    });
-  }
-  return stringNumbers;
-  return ':)';
+  numbers = numbers.map(String);
+  numbers.sort((a, b) => {
+    if (a + b > b + a) {
+      return -1;
+    } else {
+      return 1;
+    }
+  })
+  return BigInt(numbers.join('')).toString();
 }
 
 // console.log(maxSalary(5, [89, 215, 6, 7, 9]));
 // 98976215
-console.log(maxSalary(5, [42, 4, 465, 46, 427]));
+// console.log(maxSalary(5, [42, 4, 465, 46, 427]));
+// console.log(maxSalary(5, [0, 0, 0, 0, 0]));
 // 46546442742
