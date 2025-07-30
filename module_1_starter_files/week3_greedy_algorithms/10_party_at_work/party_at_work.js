ab2c863ce4343ae74be9424b5cba131b0f439cf2
theigorp <mr.igor187@gmail.com>
2025-07-30 10:40:39 +0200

    week3/party_at_work: setup files

---
+++ module_1_starter_files/week3_greedy_algorithms/10_party_at_work/party_at_work.js
@@ -0,0 +1,20 @@
+/*
+  PROBLEM STATEMENT:
+  Find a maximum independent set in a tree.
+  Input: 
+    A tree representing the hierarchical structure of a company, where each edge connects a person with her/his immediate boss.
+    The Director of Human Resources wants to invite as many people as possible. But, since some people are shy when their
+    bosses are around, she decides not to invite a person if her/his immediate boss is invited.
+  Output:
+    A maximum independent set of nodes in this tree, i.e., a node-set where every two nodes are not adjacent (the maximum
+    number of people at the party).
+
+  do this problem when you understand trees 
+*/
+function partyPlanningAtWork() {
+
+}
+
+console.log(partyPlanningAtWork());
+console.log(partyPlanningAtWork());
+console.log(partyPlanningAtWork());
