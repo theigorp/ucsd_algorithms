00707a7f584691f6b3c41dc93a094744e792e9c8
theigorp <mr.igor187@gmail.com>
2025-07-31 14:15:25 +0200

    week3/cooking_a_dinner: changed the greedy approach and fixed the problem

--- module_1_starter_files/week3_greedy_algorithms/11_cooking_a_dinner/cooking_a_dinner.js
+++ module_1_starter_files/week3_greedy_algorithms/11_cooking_a_dinner/cooking_a_dinner.js
@@ -2,26 +2,29 @@ function cookingADinner(timeToCook, freshness) {
   //* O(n)
   const dishes = timeToCook.map((time, i) => ({
     time,
-    fresh: freshness[i]
+    fresh: freshness[i],
+    deadline: time + freshness[i]
   }));
   //* O(nlogn)
-  dishes.sort((a,b) => a.time - b.time);
-  const freshRanges = [];
+  dishes.sort((a,b) => a.deadline - b.deadline);
+
   let elapsedTime = 0;
   //* O(n)
   for (const dish of dishes) {
     elapsedTime += dish.time;
-    const freshTimeStart = elapsedTime;
-    const freshTimeEnd = elapsedTime + dish.fresh;
-
-    freshRanges.push([freshTimeStart, freshTimeEnd]);
+    
+    if (elapsedTime > dish.deadline) {
+      return false;
+    }
   }
 
-  return freshRanges[0][1] >= freshRanges[freshRanges.length - 1][0];
+  return true;
 }
 
-// console.log(canAllDishesBeFresh([3,2,1], [5,4,3])); // F
-// console.log(canAllDishesBeFresh([2,3,1], [5,7,4])); // F
-// console.log(canAllDishesBeFresh([2,3,1], [6,7,5])); // T
-// console.log(canAllDishesBeFresh([2,1], [3,4])) // T
-// console.log(canAllDishesBeFresh([2,3,1], [10,3,5])) // T
+// console.log(cookingADinner([3,2,1], [5,4,3])); // F
+// console.log(cookingADinner([2,3,1], [5,7,4])); // F
+// console.log(cookingADinner([2,3,1], [6,7,5])); // T
+// console.log(cookingADinner([2,1], [3,4])) // T
+// console.log(cookingADinner([2,3,1], [10,3,5])) // T
+// console.log(cookingADinner([3,1], [1,100])) // T
+// console.log(cookingADinner([3,4], [2,1])) // F
