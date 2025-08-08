e09ebb7542690eb69f0c7e1fac8a3f6fe7c3a079
theigorp <mr.igor187@gmail.com>
2025-08-08 11:48:53 +0200

    week3/refactor: optimize solution and add explanation for LC 55

--- module_1_starter_files/week3_greedy_algorithms/leetcode/55_jump_game.js
+++ module_1_starter_files/week3_greedy_algorithms/leetcode/55_jump_game.js
@@ -2,27 +2,30 @@
   *MEDIUM
 
   LOGIC:
-  To be updated.
+  For an index position to reach the end, the sum of it's index and value must exceed or be equal to the last index. Easiest way to check this
+  is to set lastIndexToReachEnd to the last index of an array and then loop the array in reverse. if (i + nums[i] >= lastIndexToReachEnd) then
+  set lastIndexToReachEnd to current index.
 
   OPTIMIZATION:
   The runtime is O(n).
 */
 function jumpGame(nums) {
-  let lastGoodIndexPosition = nums[nums.length - 1];
-  
+  let lastIndexToReachEnd = nums[nums.length - 1];
+
   for (let i = nums.length - 1; i >= 0; i--) {
-    if (i + nums[i] >= lastGoodIndexPosition) {
-      lastGoodIndexPosition = i;
+    if (i + nums[i] >= lastIndexToReachEnd) {
+      lastIndexToReachEnd = i;
     }
   }
 
-  return lastGoodIndexPosition === 0;
+  return lastIndexToReachEnd === 0;
 }
 
-console.log(jumpGame([3, 2, 1, 0, 4])); // false
-console.log(jumpGame([2, 3, 1, 1, 4])); // true
-console.log(jumpGame([2, 0])); // true
-console.log(jumpGame([2, 0, 0])); // true
-console.log(jumpGame([0])); // true
-console.log(jumpGame([0, 1])); // false
-console.log(jumpGame([3, 0, 8, 2, 0, 0, 1])); // true
+// console.log(jumpGame([3, 2, 1, 0, 4])); // false
+// console.log(jumpGame([2, 3, 1, 1, 4])); // true
+// console.log(jumpGame([2, 0])); // true
+// console.log(jumpGame([2, 0, 0])); // true
+// console.log(jumpGame([0])); // true
+// console.log(jumpGame([0, 0, 0])); // false
+// console.log(jumpGame([0, 1])); // false
+// console.log(jumpGame([3, 0, 8, 2, 0, 0, 1])); // true
