466476b8160241f91e22f697d3d02f567866450c
theigorp <mr.igor187@gmail.com>
2025-08-08 11:52:41 +0200

    week3/refactor: correct Big O runtimes for LC 2027

--- module_1_starter_files/week3_greedy_algorithms/leetcode/2027_min_moves_to_string.js
+++ module_1_starter_files/week3_greedy_algorithms/leetcode/2027_min_moves_to_string.js
@@ -8,22 +8,24 @@
   Return the minimum number of moves required so that all the characters of s are converted to 'O'.
 
   LOGIC:
-  We will select the first three characters and check if they contain X. If yes, that means we need to convert them, therefore minMoves is icnremented. Since we "converted" those 3 characters
+  We will select the first three characters and check if they contain X. If yes, that means we need to convert them, therefore minMoves is incremented. Since we "converted" those 3 characters
   we increase counter (i) by 3 to skip visiting the indexes we already covered. In case the first three characters don't contain X, we will also increment i by 3 because we covered that
   segment of the string. An edge case is when string begins with O. In that case, we can omit the O and select the first three characters from the first occurence of X (because if we don't do that, we will make unnecessary additional moves). Therefore, in the case when string starts with O, we increment the counter (i) until we reach the first X.
 
   OPTIMIZATION:
-  The runtime is O(n).
+  The runtime is O(n^2). Optimal solution below is O(n).
 */
 function minMovesToString(string) {
   let minMoves = 0;
   let i = 0;
-
+  //* O(n)
   while (i < string.length) {
     if (string[i] === 'O') {
       i++;
     } else {
+      //* O(n)
       const move = string.substring(i, i + 3);
+      //* O(n)
       if (move.includes('X')) {
         minMoves++;
       }
