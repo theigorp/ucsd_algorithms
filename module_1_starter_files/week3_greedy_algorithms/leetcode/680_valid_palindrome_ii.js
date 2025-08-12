56443225cd5199244e9237b7aaa1366db9498eed
theigorp <mr.igor187@gmail.com>
2025-08-12 11:53:36 +0200

    week3/leetcode: optimize the existing solution for better readability

--- module_1_starter_files/week3_greedy_algorithms/leetcode/680_valid_palindrome_ii.js
+++ module_1_starter_files/week3_greedy_algorithms/leetcode/680_valid_palindrome_ii.js
@@ -8,27 +8,25 @@
   OPTIMIZATION:
   The runtime is O(n^2).
 */
-var validPalindrome = function(s) {
+function validPalindrome(s) {
     let left = 0;
     let right = s.length - 1;
 
     while (left < right) {
-        if (s[left] === s[right]) {
-            left++;
-            right--;
-        } else {
-            //* O(1) in V8 engine for String types, but O(n) for Array types
-            const withoutLeft = s.slice(0, left).concat(s.slice(left + 1));
-            const withoutRight = s.slice(0, right).concat(s.slice(right + 1));
-            const reversedWithoutLeft = withoutLeft.split('').reverse().join('');
-            const reversedWithoutRight = withoutRight.split('').reverse().join('');
+        if (s[left] !== s[right]) {
+          //* O(1) in V8 engine for String types, but O(n) for Array types
+          const withoutLeft = s.slice(0, left).concat(s.slice(left + 1));
+          const withoutRight = s.slice(0, right).concat(s.slice(right + 1));
+          const reversedWithoutLeft = withoutLeft.split('').reverse().join('');
+          const reversedWithoutRight = withoutRight.split('').reverse().join('');
 
-            if (withoutLeft === reversedWithoutLeft || withoutRight === reversedWithoutRight) {
-                return true;
-            }
-            return false;
+          return withoutLeft === reversedWithoutLeft || withoutRight === reversedWithoutRight;
         }
+        left++;
+        right--;
     }
 
     return true;
 };
+
+console.log(validPalindrome('abbca'))
