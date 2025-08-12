db0807511c3851cd66b86e888890242b20e9f83a
theigorp <mr.igor187@gmail.com>
2025-08-12 12:09:58 +0200

    week3/leetcode: add an optimized solution for LC 680

--- module_1_starter_files/week3_greedy_algorithms/leetcode/680_valid_palindrome_ii.js
+++ module_1_starter_files/week3_greedy_algorithms/leetcode/680_valid_palindrome_ii.js
@@ -9,24 +9,50 @@
   The runtime is O(n^2).
 */
 function validPalindrome(s) {
-    let left = 0;
-    let right = s.length - 1;
+  let left = 0;
+  let right = s.length - 1;
 
-    while (left < right) {
-        if (s[left] !== s[right]) {
-          //* O(1) in V8 engine for String types, but O(n) for Array types
-          const withoutLeft = s.slice(0, left).concat(s.slice(left + 1));
-          const withoutRight = s.slice(0, right).concat(s.slice(right + 1));
-          const reversedWithoutLeft = withoutLeft.split('').reverse().join('');
-          const reversedWithoutRight = withoutRight.split('').reverse().join('');
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      //* O(1) in V8 engine for String types, but O(n) for Array types
+      const withoutLeft = s.slice(0, left).concat(s.slice(left + 1));
+      const withoutRight = s.slice(0, right).concat(s.slice(right + 1));
+      const reversedWithoutLeft = withoutLeft.split('').reverse().join('');
+      const reversedWithoutRight = withoutRight.split('').reverse().join('');
 
-          return withoutLeft === reversedWithoutLeft || withoutRight === reversedWithoutRight;
-        }
-        left++;
-        right--;
+      return withoutLeft === reversedWithoutLeft || withoutRight === reversedWithoutRight;
     }
+    left++;
+    right--;
+  }
 
+  return true;
+}
+//* O(n)
+function optimizedValidPalindrome(s) {
+  function isPalindrome(left, right) {
+    while (left < right) {
+      if (s[left] !== s[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
     return true;
-};
+  }
 
-console.log(validPalindrome('abbca'))
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return isPalindrome(left + 1, right) || isPalindrome(left, right - 1);
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+console.log(validPalindrome('abbca'));
