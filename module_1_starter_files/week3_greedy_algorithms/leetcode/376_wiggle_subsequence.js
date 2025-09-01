1de2f1ed8eb061c532f345a3b2cf595b741719c9
theigorp <mr.igor187@gmail.com>
2025-09-01 07:31:38 +0200

    week3/leetcode: solve LC 376

--- module_1_starter_files/week3_greedy_algorithms/leetcode/376_wiggle_subsequence.js
+++ module_1_starter_files/week3_greedy_algorithms/leetcode/376_wiggle_subsequence.js
@@ -1,19 +1,29 @@
-var wiggleMaxLength = function(nums) {
-    let elementsToRemove = 0;
-    const differences = [];
+function wiggleSubsequence(nums) {
+  if (nums.length === 1) {
+    return nums.length;
+  }
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > nums[i + 1] || nums[i] < nums[i + 1]) {
-          
-        }
+  let up = 1;
+  let down = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    console.log('nums[i]', nums[i])
+    console.log('nums[i-1]', nums[i - 1])
+    if (nums[i] > nums[i - 1]) {
+      up = down + 1;
+      console.log('up after', up)
+    } else if (nums[i] < nums[i - 1]) {
+      down = up + 1;
+      console.log('down after', down);
     }
-    // console.log(differences)
-    // for (let i = 0; i < differences.length; i++) {
-    //     if (differences[i] >= 0 && differences[i + 1] >= 0 || differences[i] <= 0 && differences[i + 1] <= 0) {
-    //         console.log('aa')
-    //         elementsToRemove++;
-    //     }
-    // }
+  }
 
-    return nums.length - elementsToRemove;
+  return Math.max(up, down);
 };
+
+// console.log(wiggleSubsequence([1,2,3,4,5,6,7,8,9]));
+// console.log(wiggleSubsequence([0,0,0]));
+// console.log(wiggleSubsequence([1, 7, 4, 9, 2, 5]));
+// console.log(wiggleSubsequence([1, 4, 7, 2, 5]));
+console.log(wiggleSubsequence([1, 7, 4, 5, 5]));
+// console.log(wiggleSubsequence([1,17,5,10,13,15,10,5,16,8]));
